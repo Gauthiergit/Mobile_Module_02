dbbced19c64d1bfaacbefe762e5e1aedb711a844
Gauthiergit <g.p220498@gmail.com>
2026-02-17 15:24:37 +0100

    icon on Currently

--- mobileModule02/medium_weather_app/app/(tabs)/index.tsx
+++ mobileModule02/medium_weather_app/app/(tabs)/index.tsx
@@ -2,11 +2,12 @@ import { ThemedText } from '@/components/ThemedText';
 import { errorTextColor, Styles, tintColor } from '@/constants/theme';
 import { useSearchlocation } from '@/providers/SearchLocationProvider';
 import { useEffect, useState } from 'react';
-import { ActivityIndicator, View } from 'react-native';
+import { ActivityIndicator, StyleSheet, View } from 'react-native';
 import { fetchWeatherApi } from "openmeteo"
 import { CurrentWeather } from '@/types/Weather';
-import { getWeatherDescription, weatherMap } from '@/mappers/WeatherMap';
+import { getWeatherDescription, getWeatherIcon, weatherMap } from '@/mappers/WeatherMap';
 import { WEATHER_URL } from '@/constants/url';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 
 export default function CurrentlyScreen() {
 	const { location, errorMessage, setErrorMessage} = useSearchlocation();
@@ -52,11 +53,14 @@ export default function CurrentlyScreen() {
 			) : (
 				<View>
 					{curWeather && location && (
-						<View style={Styles.container}>
+						<View style={styles.container}>
 							<ThemedText type="title">{location.name}</ThemedText>
 							<ThemedText type="title">{location.admin1}</ThemedText>
 							<ThemedText type="title">{location.country}</ThemedText>
-							<ThemedText type="title">{getWeatherDescription(curWeather.weatherCode)}</ThemedText>
+							<View style={styles.description}>
+								<MaterialCommunityIcons name={getWeatherIcon(curWeather.weatherCode) as any} size={50} color={tintColor} />
+								<ThemedText type="title" style={{ textAlign: 'center' }}>{getWeatherDescription(curWeather.weatherCode)}</ThemedText>
+							</View>
 							<ThemedText type="title">{curWeather.temperature?.toFixed(2)} °C</ThemedText>
 							<ThemedText type="title">{curWeather.windSpeed?.toFixed(2)} km/h</ThemedText>
 						</View>
@@ -69,3 +73,17 @@ export default function CurrentlyScreen() {
 		</View>
 	);
 }
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		alignItems: "center",
+		justifyContent: "center",
+		gap: 10
+	},
+	description: {
+		alignItems: "center",
+		justifyContent: "center",
+		gap: 10
+	},
+});
